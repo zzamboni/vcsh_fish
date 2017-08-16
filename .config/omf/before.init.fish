1a68b7d502cbc16f4bfb2677538200adc8f43514
Diego Zamboni <diego@zzamboni.org>
2017-08-16 15:56:34 +0200

    Added omf config

--- .config/omf/before.init.fish
+++ .config/omf/before.init.fish
@@ -6,5 +6,3 @@ set -U autoproxy_check_command "test -f /etc/resolv.conf; and egrep -q '^(search
 set -U autoproxy_notifications yes
 # My paths
 set -U fish_user_paths $HOME/bin $GOPATH/bin /Users/taazadi1/Dropbox/Personal/devel/hammerspoon/spoon/bin
-# Github access token for Homebrew
-set -Ux HOMEBREW_GITHUB_API_TOKEN f8c8911faef12aabe316a62e090cd6bc927c57c5
